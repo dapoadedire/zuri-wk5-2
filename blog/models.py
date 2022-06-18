58ce0e97acb974c65a12f50cfa8081bee04c2146
dapoadedire <adedireadedapo19@gmail.com>
2022-06-18 11:17:32 +0100

    Update get_user_model

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,11 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 
-User = get_user_model()
 # Create your models here.
 class Blog(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     created_date = models.DateTimeField(auto_now_add=True)
     published_date = models.DateTimeField()
  
